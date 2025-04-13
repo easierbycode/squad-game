21595a43b0006a28da152607eb8a97ba87912a1c
▓▒░ ♔ Daniel ♔ ░▒▓ <563975+easierbycode@users.noreply.github.com>
2025-04-13 12:42:48 +0000

    clean up

--- scenes/StarScene.js
+++ scenes/StarScene.js
@@ -5,7 +5,6 @@ import { GeekCar } from "../sprites/geek-car.js";
 import { GeekStar } from "../sprites/geek-star.js";
 import { HeadphoneInvader } from "../sprites/headphone-invader.js";
 import { Laptop } from "../sprites/laptop.js";
-import { Octopus } from "../sprites/octopus.js";
 import { Player } from "../sprites/player.js"
 import { PS5Controller } from "../sprites/ps5-controller.js";
 import Stars from "../sprites/stars.js"
@@ -19,108 +18,107 @@ export class StarScene extends Phaser.Scene {
 
     preload() {
 
-        this.load.setBaseURL('https://easierbycode.github.io/squad-game/js/');
+        this.load.setBaseURL('https://easierbycode.github.io/squad-game/');
 
-        this.load.audio('audio_beam', '../assets/sounds/beam.mp3')
-        this.load.audio('audio_explosion', '../assets/sounds/explosion.mp3')
-        this.load.audio('audio_pickup', '../assets/sounds/pickup.mp3')
-        this.load.audio('rudy_saved', '../assets/sounds/rudy-saved.mp3')
-        this.load.image( 'bby-ship', '../assets/images/best-buy-ship.png')
-        this.load.image( 'blue', '../assets/images/blue.png')
-        this.load.image( 'geek-bullet', '../assets/images/geek-bullet.png')
-        this.load.image( 'geek-car', '../assets/images/geek-car.png')
-        this.load.image( 'geek-e', '../assets/images/geek-e.png')
-        this.load.image( 'geek-g', '../assets/images/geek-g.png')
-        this.load.image( 'geek-k', '../assets/images/geek-k.png')
-        this.load.image( 'geekstar-dead', '../assets/images/geekstar-dead.png')
-        this.load.image( 'geekstar-grey', '../assets/images/geekstar-grey.png')
-        this.load.image('octopus-particles-0', '../assets/images/octopus-particles-0.png')
-        this.load.image('octopus-particles-1', '../assets/images/octopus-particles-1.png')
-        this.load.image( 'red', '../assets/images/red.png')
-        this.load.image('squad-s', '../assets/images/squad-s.png')
-        this.load.image('squad-q', '../assets/images/squad-q.png')
-        this.load.image('squad-u', '../assets/images/squad-u.png')
-        this.load.image('squad-a', '../assets/images/squad-a.png')
-        this.load.image('squad-d', '../assets/images/squad-d.png')
-        // this.load.image( 'rudy', '../assets/images/rudy.png' )
-        this.load.image( 'star', '../assets/images/star.png' )
-        this.load.spritesheet('beam', '../assets/spritesheets/sperm.png', {
+        this.load.audio('audio_beam', 'assets/sounds/beam.mp3')
+        this.load.audio('audio_explosion', 'assets/sounds/explosion.mp3')
+        this.load.audio('audio_pickup', 'assets/sounds/pickup.mp3')
+        this.load.audio('rudy_saved', 'assets/sounds/rudy-saved.mp3')
+        this.load.image( 'bby-ship', 'assets/images/best-buy-ship.png')
+        this.load.image( 'blue', 'assets/images/blue.png')
+        this.load.image( 'geek-bullet', 'assets/images/geek-bullet.png')
+        this.load.image( 'geek-car', 'assets/images/geek-car.png')
+        this.load.image( 'geek-e', 'assets/images/geek-e.png')
+        this.load.image( 'geek-g', 'assets/images/geek-g.png')
+        this.load.image( 'geek-k', 'assets/images/geek-k.png')
+        this.load.image( 'geekstar-dead', 'assets/images/geekstar-dead.png')
+        this.load.image( 'geekstar-grey', 'assets/images/geekstar-grey.png')
+        this.load.image('octopus-particles-0', 'assets/images/octopus-particles-0.png')
+        this.load.image('octopus-particles-1', 'assets/images/octopus-particles-1.png')
+        this.load.image( 'red', 'assets/images/red.png')
+        this.load.image('squad-s', 'assets/images/squad-s.png')
+        this.load.image('squad-q', 'assets/images/squad-q.png')
+        this.load.image('squad-u', 'assets/images/squad-u.png')
+        this.load.image('squad-a', 'assets/images/squad-a.png')
+        this.load.image('squad-d', 'assets/images/squad-d.png')
+        this.load.image( 'star', 'assets/images/star.png' )
+        this.load.spritesheet('beam', 'assets/spritesheets/sperm.png', {
             frameWidth: 6,
             frameHeight: 22
         })
-        this.load.spritesheet('beam-impact', '../assets/spritesheets/sperm-impact.png', {
+        this.load.spritesheet('beam-impact', 'assets/spritesheets/sperm-impact.png', {
             frameWidth: 18,
             frameHeight: 28
         })
-        this.load.spritesheet('cyber-octopus', '../assets/spritesheets/cyber-octopus.png', {
+        this.load.spritesheet('cyber-octopus', 'assets/spritesheets/cyber-octopus.png', {
             frameWidth: 29,
             frameHeight: 29
         })
-        this.load.spritesheet('explosion', '../assets/spritesheets/explosion.png', {
+        this.load.spritesheet('explosion', 'assets/spritesheets/explosion.png', {
             frameWidth: 16,
             frameHeight: 16
         })
-        this.load.spritesheet('geek-ellipsis', '../assets/spritesheets/geek-ellipsis.png', {
+        this.load.spritesheet('geek-ellipsis', 'assets/spritesheets/geek-ellipsis.png', {
             frameWidth: 15,
             frameHeight: 10
         })
-        this.load.spritesheet('geekstar', '../assets/spritesheets/gs-logo.png', {
+        this.load.spritesheet('geekstar', 'assets/spritesheets/gs-logo.png', {
             frameWidth: 170,
             frameHeight: 105
         })
-        this.load.spritesheet('headphone-invader', '../assets/spritesheets/headphone-invader.png', {
+        this.load.spritesheet('headphone-invader', 'assets/spritesheets/headphone-invader.png', {
             frameWidth: 191,
             frameHeight: 142
         })
-        this.load.spritesheet('headphone-invader-particles', '../assets/spritesheets/headphone-invader-particles.png', {
+        this.load.spritesheet('headphone-invader-particles', 'assets/spritesheets/headphone-invader-particles.png', {
             frameWidth: 45,
             frameHeight: 45
         })
-        this.load.spritesheet('laptop', '../assets/spritesheets/laptop.png', {
+        this.load.spritesheet('laptop', 'assets/spritesheets/laptop.png', {
             frameWidth: 74,
             frameHeight: 58
         })
-        this.load.spritesheet('laptop-particles', '../assets/spritesheets/laptop-particles.png', {
+        this.load.spritesheet('laptop-particles', 'assets/spritesheets/laptop-particles.png', {
             frameWidth: 2,
             frameHeight: 2
         })
-        this.load.spritesheet('ms-logo-particles', '../assets/spritesheets/ms-logo-particles.png', {
+        this.load.spritesheet('ms-logo-particles', 'assets/spritesheets/ms-logo-particles.png', {
             frameWidth: 10,
             frameHeight: 8
         })
-        this.load.spritesheet('octopus', '../assets/spritesheets/octopus.png', {
+        this.load.spritesheet('octopus', 'assets/spritesheets/octopus.png', {
             frameWidth: 21,
             frameHeight: 24
         })
-        this.load.spritesheet('octopus-particles', '../assets/spritesheets/octopus-particles.png', {
+        this.load.spritesheet('octopus-particles', 'assets/spritesheets/octopus-particles.png', {
             frameWidth: 16,
             frameHeight: 29
         })
-        this.load.spritesheet('password-wrong-particles', '../assets/spritesheets/password-wrong-particles.png', {
+        this.load.spritesheet('password-wrong-particles', 'assets/spritesheets/password-wrong-particles.png', {
             frameWidth: 2,
             frameHeight: 2
         })
-        this.load.spritesheet('ps5-bullet', '../assets/spritesheets/ps5-bullet.png', {
+        this.load.spritesheet('ps5-bullet', 'assets/spritesheets/ps5-bullet.png', {
             frameWidth: 32,
             frameHeight: 32
         })
-        this.load.spritesheet('ps5-controller', '../assets/spritesheets/ps5-controller.png', {
+        this.load.spritesheet('ps5-controller', 'assets/spritesheets/ps5-controller.png', {
             frameWidth: 22,
             frameHeight: 13
         })
-        this.load.spritesheet('ps5-controller-particles', '../assets/spritesheets/ps5-controller-particles.png', {
+        this.load.spritesheet('ps5-controller-particles', 'assets/spritesheets/ps5-controller-particles.png', {
             frameWidth: 2,
             frameHeight: 2
         })
-        this.load.spritesheet('rudy', '../assets/spritesheets/rudy.png', {
+        this.load.spritesheet('rudy', 'assets/spritesheets/rudy.png', {
             frameWidth: 17,
             frameHeight: 29
         })
-        this.load.spritesheet( 'ship-boy', '../assets/spritesheets/ship-boy.png', {
+        this.load.spritesheet( 'ship-boy', 'assets/spritesheets/ship-boy.png', {
             frameWidth: 117,
             frameHeight: 142
         })
-        this.load.spritesheet( 'wrong-password', '../assets/spritesheets/wrong-password.png', {
+        this.load.spritesheet( 'wrong-password', 'assets/spritesheets/wrong-password.png', {
             frameWidth: 30,
             frameHeight: 22
         })
@@ -178,8 +176,6 @@ export class StarScene extends Phaser.Scene {
 
         this.sentinels = this.add.group()
 
-        // this.sentinels.active = true
-
         this.geekCar = new GeekCar(this, -587, 35)
         this.sentinels.add( this.geekCar )
 
@@ -195,15 +191,7 @@ export class StarScene extends Phaser.Scene {
 
         // ENEMIES
 
-        // create a grid of 7 enemies spaced evenly
-        // for (var i = 0; i < 7; i++) {
-        //     let y = -50;
-        //     if (i % 2 == 0)  y = -98;
-
-        //     let o = new Octopus(this, 30 + (i * 50), y);
-
-        //     this.sentinels.add( o );
-        // }
+        // create a grid of CyberOctopus spaced evenly
         for (var i = 0; i < 5; i++) {
             let y = -50;
             if (i % 2 == 0)  y = -98;
@@ -230,7 +218,7 @@ export class StarScene extends Phaser.Scene {
         this.controllersLaunched = false;
     }
 
-    // //  loop which runs continuously
+    // loop which runs continuously
     update() {
         if (this.player.alpha >= 0.3) {
             this.player.movePlayerManager();
